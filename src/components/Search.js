a6fe390469a0ac4bbbe970182d77acd63c7e794b
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-08 19:23:27 +0100

    form and requests done

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,35 @@ import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const { requests } = React.useContext(GithubContext);
+  const [user, setUser] = React.useState('');
+
+  // get things from global context
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    if (user) {
+    }
+  };
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="enter github user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            {requests > 0 && <button type="submit">search</button>}
+          </div>
+        </form>
+        <h3>requests: {requests} / 60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -12,9 +12,32 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0);
+  const [loading, setLoading] = useState(false);
+  // error
+
+  const checkRequest = async () => {
+    try {
+      const { data } = await axios(`${rootUrl}/rate_limit`);
+      const {
+        rate: { remaining },
+      } = data;
+      setRequests(remaining);
+      if (remaining === 0) {
+        // throw error
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // error
+
+  useEffect(() => {
+    checkRequest();
+  }, []);
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
       {children}
     </GithubContext.Provider>
   );
